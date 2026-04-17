9faaeddcf7a2709b9901f870fd255d4a71bd1f32
Julian Graf <julian.jg.graf@gmail.com>
2026-04-17 19:52:33 +0200

    improve QoS documentation

--- src/types/qos.rs
+++ src/types/qos.rs
@@ -1,14 +1,29 @@
 use crate::types::PacketIdentifier;
 
-/// MQTT's Quality of Service
+/// MQTT's Quality of Service levels
 #[derive(Debug, Clone, Copy, PartialEq, PartialOrd, Eq, Ord)]
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 pub enum QoS {
-    /// Quality of Service Level 0. Publications with this level are only sent once.
+    /// Quality of Service Level 0. Reliable delivery of publications at this level is guaranteed solely
+    /// by the underlying transport mechanism within a continuous network connection. Publications can
+    /// be lost if a disconnection on protocol or transport level occurs around the transmission time.
+    /// A single message is never delivered more than once. Retransmissions cannot be marked as such on
+    /// protocol level.
     AtMostOnce = 0,
-    /// Quality of Service Level 1. Publications with this level are sent until a PUBACK indicates that it was received.
+
+    /// Quality of Service Level 1. Reliable delivery of publications is guaranteed at the cost of a
+    /// two-way handshake on protocol level, even with disconnects around (re-)transmission time,
+    /// given a reconnection to the same, non-expired session is successful and a retransmission is
+    /// subsequently attempted. A single message can be delivered more than once, but actual duplicates
+    /// cannot reliably be distinguished from needed retransmissions.
     AtLeastOnce = 1,
-    /// Quality of Service Level 2. Publications with this level are followed by a handshake assuring it is received once.
+
+    /// Quality of Service Level 2. Reliable, exactly-once delivery of publications is guaranteed at
+    /// the cost of a four-way handshake at protocol level, even with disconnects around
+    /// (re-)transmission time, given a reconnection to the same, non-expired session is successful and
+    /// retransmission of the PUBLISH or PUBREL packets is subsequently attempted. Delivery of
+    /// publications may be further delayed than at other quality of service levels due to the required
+    /// release of the message after the three-way handshake within the four-way handshake.
     ExactlyOnce = 2,
 }
 impl From<IdentifiedQoS> for QoS {
