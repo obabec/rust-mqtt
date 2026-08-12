087ec50ac86c6a14057f4f2ff49994efeff21694
Julian Graf <julian.jg.graf@gmail.com>
2026-08-13 00:34:31 +0200

    allow any valid disconnect reason code

--- src/client/options/disconnect.rs
+++ src/client/options/disconnect.rs
@@ -2,18 +2,24 @@ use const_fn::const_fn;
 
 use crate::{
     config::SessionExpiryInterval,
-    types::{MqttString, MqttStringPair},
+    types::{MqttString, MqttStringPair, ReasonCode},
 };
 
 /// Options for a disconnection to the server with a DISCONNECT packet.
 #[derive(Debug, Clone)]
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 pub struct Options<'d> {
-    /// If set to true, the client uses [`ReasonCode::DisconnectWithWillMessage`] in the
-    /// DISCONNECT packet and the server publishes the will message.
+    /// The [`ReasonCode`] of the DISCONNECT packet.
     ///
-    /// [`ReasonCode::DisconnectWithWillMessage`]: crate::types::ReasonCode::DisconnectWithWillMessage
-    pub publish_will: bool,
+    /// Must be one of [`ReasonCode::Success`], [`ReasonCode::DisconnectWithWillMessage`],
+    /// [`ReasonCode::UnspecifiedError`], [`ReasonCode::MalformedPacket`],
+    /// [`ReasonCode::ProtocolError`], [`ReasonCode::ImplementationSpecificError`],
+    /// [`ReasonCode::TopicNameInvalid`], [`ReasonCode::ReceiveMaximumExceeded`],
+    /// [`ReasonCode::TopicAliasInvalid`], [`ReasonCode::PacketTooLarge`],
+    /// [`ReasonCode::MessageRateTooHigh`], [`ReasonCode::QuotaExceeded`],
+    /// [`ReasonCode::AdministrativeAction`] or [`ReasonCode::PayloadFormatInvalid`]
+    /// (Compare [Disconnect Reason Code](https://docs.oasis-open.org/mqtt/mqtt/v5.0/os/mqtt-v5.0-os.html#_Toc3901208), \[MQTT-3.14.2-1\]).
+    pub reason_code: ReasonCode,
 
     /// The session expiry interval property. This value overrides the session expiry interval
     /// negotiated in the handshake.
@@ -44,22 +50,38 @@ impl Default for Options<'_> {
 }
 
 impl<'d> Options<'d> {
-    /// Creates new disconnect options with will publication disabled and no session expiry interval.
+    /// Creates new disconnect options with will publication disabled
+    /// ([`ReasonCode::Success`] aka Normal Disconnection) and no session
+    /// expiry interval.
     #[must_use]
     pub const fn new() -> Self {
         Self {
-            publish_will: false,
+            reason_code: ReasonCode::Success,
             session_expiry_interval: None,
             reason_string: None,
             user_properties: &[],
         }
     }
 
-    /// Sets the publish will flag to true.
+    /// Sets the reason code.
+    ///
+    /// Must be one of [`ReasonCode::Success`], [`ReasonCode::DisconnectWithWillMessage`],
+    /// [`ReasonCode::UnspecifiedError`], [`ReasonCode::MalformedPacket`],
+    /// [`ReasonCode::ProtocolError`], [`ReasonCode::ImplementationSpecificError`],
+    /// [`ReasonCode::TopicNameInvalid`], [`ReasonCode::ReceiveMaximumExceeded`],
+    /// [`ReasonCode::TopicAliasInvalid`], [`ReasonCode::PacketTooLarge`],
+    /// [`ReasonCode::MessageRateTooHigh`], [`ReasonCode::QuotaExceeded`],
+    /// [`ReasonCode::AdministrativeAction`] or [`ReasonCode::PayloadFormatInvalid`]
+    /// (Compare [Disconnect Reason Code](https://docs.oasis-open.org/mqtt/mqtt/v5.0/os/mqtt-v5.0-os.html#_Toc3901208), \[MQTT-3.14.2-1\]).
     #[must_use]
-    pub const fn publish_will(mut self) -> Self {
-        self.publish_will = true;
+    pub const fn reason_code(mut self, reason_code: ReasonCode) -> Self {
+        self.reason_code = reason_code;
         self
+    }
+    /// Sets the reason code to [`ReasonCode::DisconnectWithWillMessage`].
+    #[must_use]
+    pub const fn publish_will(self) -> Self {
+        self.reason_code(ReasonCode::DisconnectWithWillMessage)
     }
     /// Sets the session expiry interval property.
     #[must_use]
